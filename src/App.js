90546e81f480d46a36b6b4ffd67971de3c7d62e9
rayenkekli1 <keklirayen5@gmail.com>
2022-09-18 16:44:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,163 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+  <div className='main'>
+      <div className='navbar'>
+        <img src='https://www.racecar-engineering.com/wp-content/uploads/2018/03/F1-LOGO.png'></img>
+        <nav>
+          <ul>
+            <li><a href="#">cars</a></li>
+            <li><a href="#">drivers</a></li>
+            <li><a href="#">news</a></li>
+          </ul>
+        </nav>
+      </div>
+    <div className='intorduction'>
+        <h1>The 3 Greatest Formula 1 Cars Of All Time</h1>
+        <br></br>
+        <br></br>
+      <h3>Formula 1 has been around for 70 years now – in case you hadn’t heard – and there have been some remarkable machines competing in that time. Companies have invested millions each year on their race cars alone.
+      <br></br>
+      Companies, sometimes, have a budget upward of $400 million dollars per season. With all those resources and cash, it's no wonder why F1 cars are some of the most technologically advanced vehicles on the planet.
+      <br></br>
+        There have been some amazing machines built in this time, which have made a reputation for taking win after win and dominating the competition.<br></br>
+        <br></br>To illustrate this point, here are three of best F1 cars ever built:</h3>
+    </div>
+    <div  className='car1'>
+      <h1>1. Mercedes AMG F1 W07 Hybrid:</h1>
+        <img src='https://prod.chaseyoursport.com/blogImage/0.1619118858874753.jpeg'></img>
+        <div className='cardetails1'>
+          <h3>Spécifications techniques:</h3><br></br>
+          <h5>Châssis:             <span>Monocoque composite en fibre de carbone</span></h5>
+          <h5>Suspension avant:    <span>Doubles triangles superposés, poussoirs et basculeurs avec barre de torsion</span></h5>
+          <h5>Suspension arrière:  <span>Doubles triangles superposés, tirants et basculeurs avec barre de torsion</span></h5>
+          <h5>Nom du moteur:       <span>Mercedes PU106C hybride turbo</span></h5>
+          <h5>Cylindrée:           <span>1 600 cm3</span></h5>
+          <h5>Configuration:       <span>V6 turbocompressé ouvert à 90°, limité à 15 000 tr/min</span></h5>
+          <h5>Orientation du moteur:<span>Longitudinal</span></h5>
+          <h5>Position du moteur:   <span>Centrale arrière</span></h5>
+          <h5>Boîte de vitesses:    <span>Mercedes, séquentielle à contrôle électro-hydraulique seamless, en fibre de carbone composite</span></h5>
+          <h5>Nombre de rapports:   <span>8 + marche arrière, système Quickshift</span></h5>
+          <h5>Électronique:         <span>Boîtier électronique homologué FIA</span></h5>
+          <h5>Système de carburant: <span>Réservoir de 100 kg d'essence</span></h5>
+          <h5>Système de freinage:  <span>Disques et étriers Brembo Disques et étriers Carbone Industrie</span></h5>
+          <h5>Poids:                <span>702 kg avec le pilote, les caméras et le lest</span></h5>
+          <h5>Dimensions:           <span>longueur : 5000mm, largeur : 1800mm, hauteur : 950mm.</span></h5>
+          <h5>Carburant:            <span>Petronas</span></h5>
+          <h5>Pneumatiques:         <span>Pirelli PZero</span></h5>
+          <h5>Partenaires:          <span>Petronas, BlackBerry</span></h5>
+        </div>
+        <div className='driverofthecar1'>
+            <h1>Lewis Hamilton</h1>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg'></img>
+            <div className='stat1'>
+              <h5>Nom complet:           <span>Lewis Carl Davidson Hamilton</span></h5>
+              <h5> Surnom :              <span>King Lewis</span></h5>
+              <h5>  Date de naissance:   <span>	7 janvier 1985 (37 ans)</span></h5>
+              <h5>  Lieu de naissance:	  <span>Angleterre</span></h5>
+              <h5>  Nationalité:	        <span>Britannique</span></h5>
+              <h5>Numéro permanent:      <span>	44</span></h5> 
+              <h5>Nombre de courses:     <span>	297</span></h5>
+              <h5>Pole positions:        <span>	103 (record)</span></h5>
+              <h5>Meilleurs tours en course:  <span>	59</span></h5>
+              <h5>Podiums:                   <span>	188 (record)</span></h5>
+              <h5>Victoires:                 <span>	103 (record)</span></h5>
+              <h5>Champion du monde:         <span>	7 (record co-détenu avec Michael Schumacher)</span></h5>
+            </div>
+        </div>
+    </div>
+      <div className='line'></div>
+
+<div  className='car2'>
+            <h1>2. Ferrari    F2002:</h1>
+            <img src='https://cdna.artstation.com/p/assets/images/images/002/490/038/large/yuri-rodchenko-2.jpg?1462363924&dl=1'></img>
+          <div className='cardetails2'>
+            <h3>Spécifications techniques:</h3><br></br>
+            <h5>Châssis:             <span>	Monocoque moulée en fibre de carbone et d'aluminium en nid d'abeille</span></h5>
+            <h5>Suspension avant:    <span>Triangles superposés, activation des ressorts par un poussoir, amortisseurs Sachs</span></h5>
+            <h5>Suspension arrière:  <span>Triangles superposés, activation des ressorts par un poussoir, amortisseurs Sachs</span></h5>
+            <h5>Nom du moteur:       <span>Ferrari Type 051</span></h5>
+            <h5>Cylindrée:           <span>2 997 cm3855 ch à 18 600 tr/min</span></h5>
+            <h5>Configuration:       <span>	V10 à 90°</span></h5>
+            <h5>Orientation du moteur:<span>Longitudinal</span></h5>
+            <h5>Position du moteur:   <span>arrière</span></h5>
+            <h5>Boîte de vitesses:    <span>	en titane, Ferrari, électronique séquentielle semi-automatique</span></h5>
+            <h5>Nombre de rapports:   <span>7 + marche arrière</span></h5>
+            <h5>Système de carburant: <span>Réservoir 140 litres</span></h5>
+            <h5>Système de freinage:  <span>	Disques et étriers Brembo</span></h5>
+            <h5>Poids:                <span>600 kg</span></h5>
+            <h5>Dimensions:           <span>Empattement : 3020 mm Voie avant : 1470 mm Voie arrière : 1405 mm</span></h5>
+            <h5>Carburant:            <span>	Shell V-Power</span></h5>
+            <h5>Pneumatiques:         <span>	Bridgestone</span></h5>
+            <h5>Partenaires:          <span>	Marlboro, Vodafone</span></h5>
+          </div>
+          <div className='driverofthecar2'>
+              <h1>Michael Schumacher</h1>
+              <img src='https://upload.wikimedia.org/wikipedia/commons/c/c6/Michael_Schumacher_2005_United_States_GP_%2819872855%29_%28cropped%29.jpg'></img>
+              <div className='stat2'>
+                <h5>Nom complet:           <span>Michael Schumacher</span></h5>
+                <h5> Surnom :              <span>le baron rouge</span></h5>
+                <h5>  Date de naissance:   <span>	3 janvier 1969 (53 ans)</span></h5>
+                <h5>  Lieu de naissance:	  <span>Hürth-Hermülheim</span></h5>
+                <h5>  Nationalité:	        <span>Allemand</span></h5>
+                <h5>Nombre de courses:     <span>	307</span></h5>
+                <h5>Pole positions:        <span>	68</span></h5>
+                <h5>Meilleurs tours en course:  <span>77 (record)</span></h5>
+                <h5>Podiums:                   <span>	155</span></h5>
+                <h5>Victoires:                 <span>		91</span></h5>
+                <h5>Champion du monde:         <span>	7 (co-recordman)</span></h5>
+            </div>
+          </div>
+        </div>
+
+        <div className='line1'></div>
+
+<div  className='car3'>
+            <h1>3. Red Bull RB9:</h1>
+            <img src='https://c4.wallpaperflare.com/wallpaper/670/478/536/2013-red-bull-rb9-wallpaper-preview.jpg'></img>
+          <div className='cardetails3'>
+            <h3>Spécifications techniques:</h3><br></br>
+            <h5>Châssis:             <span>		Monocoque composite moulée en fibre de carbone et aluminium en nid d'abeille</span></h5>
+            <h5>Suspension avant:    <span>	Doubles triangles superposés, poussoirs et basculeurs avec barre de torsion, amortisseurs Multimatic</span></h5>
+            <h5>Suspension arrière:  <span>	Doubles triangles superposés, tirants et basculeurs avec barre de torsion, amortisseurs Multimatic</span></h5>
+            <h5>Nom du moteur:       <span>Renault RS27-2013</span></h5>
+            <h5>Cylindrée:           <span>	2 400 cm3</span></h5>
+            <h5>Configuration:       <span>	V8 à 90°, limité à 18 000 tr/min</span></h5>
+            <h5>Orientation du moteur:<span>Moteur longitudinal en position arrière</span></h5>
+            <h5>Boîte de vitesses:    <span>		Red Bull, séquentielle à contrôle électro-hydraulique seamless, en position longitudinale</span></h5>
+            <h5>Nombre de rapports:   <span>7 + marche arrière, système Quickshift</span></h5>
+            <h5>Système de freinage:  <span>	Disques autoventilés et étriers Brembo</span></h5>
+            <h5>Poids:                <span>642 kg avec le pilote, les caméras et le lest</span></h5>
+            <h5>Carburant:            <span>	S	Total</span></h5>
+            <h5>Pneumatiques:         <span>	Pirelli PZero montés sur jantes OZ Racing usinées en magnésium</span></h5>
+            <h5>Partenaires:          <span>		Infiniti, Platform Computing, Siemens, ANSYS</span></h5>
+          </div>
+          <div className='driverofthecar3'>
+              <h1>Sebastian Vettel</h1>
+              <img src='https://i.pinimg.com/originals/bb/c9/71/bbc971586e6f619d41741a54295cb1e1.jpg'></img>
+              <div className='stat3'>
+                <h5>Nom complet:           <span>Sebastian Vettel</span></h5>
+                <h5> Surnom :              <span>Baby Schumi</span></h5>
+                <h5>  Date de naissance:   <span>		3 juillet 1987 (35 ans)</span></h5>
+                <h5>  Lieu de naissance:	  <span>	Heppenheim (Allemagne)</span></h5>
+                <h5>  Nationalité:	        <span>Allemand</span></h5>
+                <h5>Nombre de courses:     <span>		290</span></h5>
+                <h5>Pole positions:        <span>		57</span></h5>
+                <h5>Meilleurs tours en course:  <span>38</span></h5>
+                <h5>Podiums:                   <span>		122</span></h5>
+                <h5>Victoires:                 <span>		53</span></h5>
+                <h5>Champion du monde:         <span> 4 </span></h5>
+            </div>
+          </div>
+        </div>
+
+
+    </div>
+    );
+  }
+
+export default App;
